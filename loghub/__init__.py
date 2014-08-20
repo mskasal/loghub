2c569390d459b0f78eb397c8a0212809c16adc56
nejdetckenobi_ <nejdetyucesoy@gmail.com>
2014-08-20 18:15:02 +0300

    "mod_wsgi script created. Initializing for applications and logs fixed."

--- loghub/__init__.py
+++ loghub/__init__.py
@@ -3,4 +3,4 @@ from flask.ext.mail import Mail
 app = Flask(__name__)
 mail = Mail(app)
 from loghub import modules
-from loghub.routes import users, alarms
+from loghub.routes import users, alarms, applications, logs 
