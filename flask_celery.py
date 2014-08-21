64461f82ec00a6b9bf92997dce201ed9b197dc85
nejdetckenobi_ <nejdetyucesoy@gmail.com>
2014-08-21 11:23:01 +0300

    fixed non-standard http headers. (X-headers)

--- flask_celery.py
+++ flask_celery.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 
 
-loghub_worker = Celery("loghub_worker", broker="amqp://localhost//",
-                    backend= 'mongodb://localhost//',
+loghub_worker = Celery("loghub_worker", broker="amqp://loghub//",
+                    backend= 'mongodb://loghub//',
                     include= ['loghub.modules.users','loghub.modules.alarms','loghub.modules.applications','loghub.modules.logs']
                     )
 
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
 from loghub import app
 from loghub.storage import db
-from loghub.modules import *
-from loghub.routes import *
+
 
 if __name__ == '__main__':
     #app.run("192.168.33.10",
